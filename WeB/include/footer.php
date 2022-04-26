fea7f242fb7eec75cdd9fd4383d7f69095e66946
abhishekpaneru7 <abhishek.paneru.88@gmail.com>
2022-04-26 14:07:21 +0545

    push

--- WeB/include/footer.php
+++ WeB/include/footer.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Font Awesome -->
-    <!-- <link
+    <link
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
     rel="stylesheet"
-    /> -->
+    />
     <!-- Google Fonts -->
     <!-- <link
     href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
