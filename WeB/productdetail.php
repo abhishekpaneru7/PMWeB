f8b5882a399e649d950d9c9f6e89efb87ac528d7
abhishekpaneru7 <abhishek.paneru.88@gmail.com>
2022-05-05 18:01:48 +0545

    ProDuct DEtails DONE

--- WeB/productdetail.php
+++ WeB/productdetail.php
@@ -11,7 +11,25 @@
     <br>
     <br>
     <br>
-    PRODUCT DETAIL
+    <div class="product-detail">
+        <h1 class="text-center">PRODUCT</h1>
+        <div class="flex-items">
+            <div class="product-img">
+                <img src="img/pih.png" alt="" width="360px" height="480px">
+            </div>
+            <div class="product-details">
+                <h5>PRODUCT DETAILS:</h5>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo minima deleniti, fugiat praesentium tenetur voluptatem error provident sequi. Eaque, nostrum et! Neque, ullam vel. Id, earum. Similique eum sunt maiores aperiam unde mollitia, possimus, quasi sed voluptatibus, delectus a. Qui eveniet eligendi voluptas debitis nostrum similique expedita rerum, maiores saepe molestias quam sapiente iusto tempore magnam soluta sit obcaecati. Nulla dolorem dignissimos perferendis fugit quis assumenda eveniet debitis harum provident cumque praesentium deleniti illum, nisi porro distinctio quaerat ut explicabo natus numquam et alias quisquam libero? Harum, dolor reprehenderit esse officia libero eaque necessitatibus recusandae, dicta aliquid repudiandae quaerat molestias?</p>
+                <h3>PRICE : $450</h3>
+                <h4>45 REMAINING IN STOCK</h4>
+                <div class="flex-items">
+                <a href="" class="btn btn-primary">Add to Card</a>
+                &nbsp;
+                <a href="" class="btn btn-danger">Buy Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php include "include/footer.php"; ?>
 </body>
 </html>
