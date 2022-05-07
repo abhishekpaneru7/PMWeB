5e5e115c68c1ff92cbce767e95412ddb8447f2a6
abhishekpaneru7 <abhishek.paneru.88@gmail.com>
2022-05-07 12:40:12 +0545

    FE basic COMP

--- WeB/search.php
+++ WeB/search.php
@@ -21,6 +21,63 @@
             </form>
         </div>
     </div>
+    <div class="search-bar">
+      <div class="flex-search">
+        <div class="side-cat-search">
+          <h3>SORT BY:</h3>
+          <hr>
+          <h5 class="text-center">ALPHABET</h5>
+          &nbsp; &nbsp; &nbsp; &nbsp;
+          <input type="radio" name="alphabet" value="AESC">
+          <label for="AESC"> AESC </label>
+          &nbsp; &nbsp; &nbsp; &nbsp;
+          <input type="radio" name="alphabet" value="DESC">
+          <label for="AESC"> DESC </label>
+          <br>
+          <br>
+          <h5 class="text-center">PRICE</h5>
+          <div class="flex-search">
+            <input type="number" placeholder="Min-Price" style="width: 120px;">
+            <span>&#8594;</span>
+            <input type="number" placeholder="Max-Price" style="width: 120px;">
+          </div>
+        </div>
+        <div class="main-cat-item">
+        <div class="container-fluid">
+  <h1 class="text-center pb-4 mt-4" >PRODUCTS</h1>
+  <div class="col-10 mx-auto">
+  <div class="row text-center">
+    
+      <div class="card col-lg-4 py-2 m-2 mx-auto" style="width: 18rem;">
+        <img class="card-img-top" src="img/pih.png" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Place Order</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eaque aspernatur quam repudiandae commodi dolorum!</p>
+        </div>
+        <span>&#8594;</span>
+      </div>
+      <div class="card col-lg-4 py-2 m-2 mx-auto" style="width: 18rem;">
+        <img class="card-img-top" src="img/pih.png" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Make Payment</h5>
+          <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim dicta dolorum reprehenderit reiciendis impedit distinctio.</p>
+        </div>
+        <span>&#8594;</span>
+      </div>
+      <div class="card col-lg-4 py-2 m-2 mx-auto" style="width: 18rem;">
+        <img class="card-img-top" src="img/pih.png " alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Collect Order</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit amet, ullam explicabo tenetur possimus illo.</p>
+        </div>
+        <span>&#8594;</span>
+      </div>
+    </div>
+  </div>
+</div>
+        </div>
+      </div>
+    </div>
     <?php include "include/footer.php"; ?>
 </body>
 </html>
--- WeB/shop.php
+++ WeB/shop.php
@@ -13,78 +13,63 @@
     <br>
     <br>
     <br>
-    <div class="container">
-      <h1 class="text-center pb-4" style="margin-top: 3%;">Our Products</h1>
-      <div class="col-12 mx-auto">
-      <div class="row">
-    <div class="card  col-lg-3 py-2 m-2 mx-auto" style="width: 18rem;">
-      <img class="card-img-top" src="img/pih.png" alt="Card image cap" height="60%">
-      <div class="card-body">
-        <h5 class="card-title">Cat1</h5>
-        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum culpa autem dolores eaque sapiente ad.</p>
-        <a href="productdetail.php" class="btn btn-primary text-center">Explore</a>
+    <div class="shop-container">
+      <div class="shop-flex">
+        <div class="sort-shop">
+          <h3>SORT BY:</h3>
+          <hr>
+          <h5 class="text-center">ALPHABET</h5>
+          &nbsp; &nbsp; &nbsp; &nbsp;
+          <input type="radio" name="alphabet" value="AESC">
+          <label for="AESC"> AESC </label>
+          &nbsp; &nbsp; &nbsp; &nbsp;
+          <input type="radio" name="alphabet" value="DESC">
+          <label for="AESC"> DESC </label>
+          <br>
+          <br>
+          <h5 class="text-center">PRICE</h5>
+          <div class="flex-search">
+            <input type="number" placeholder="Min-Price" style="width: 120px;">
+            <span>&#8594;</span>
+            <input type="number" placeholder="Max-Price" style="width: 120px;">
+          </div>
+        </div>
+        <div class="shop-products">
+        <div class="container-fluid">
+  <h1 class="text-center pb-4 mt-4" >PRODUCTS</h1>
+  <div class="col-10 mx-auto">
+  <div class="row text-center">
+    
+      <div class="card col-lg-4 py-2 m-2 mx-auto" style="width: 18rem;">
+        <img class="card-img-top" src="img/pih.png" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Place Order</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eaque aspernatur quam repudiandae commodi dolorum!</p>
+        </div>
+        <span>&#8594;</span>
       </div>
-    </div>
-    <div class="card  col-lg-3 py-2 m-2 mx-auto" style="width: 18rem;">
-      <img class="card-img-top" src="img/pih.png" alt="Card image cap" height="60%">
-      <div class="card-body">
-        <h5 class="card-title">Cat2</h5>
-        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum culpa autem dolores eaque sapiente ad.</p>
-        <a href="#" class="btn btn-primary text-center">Explore</a>
+      <div class="card col-lg-4 py-2 m-2 mx-auto" style="width: 18rem;">
+        <img class="card-img-top" src="img/pih.png" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Make Payment</h5>
+          <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim dicta dolorum reprehenderit reiciendis impedit distinctio.</p>
+        </div>
+        <span>&#8594;</span>
       </div>
-    </div>
-    <div class="card  col-lg-3 py-2 m-2 mx-auto" style="width: 18rem;">
-      <img class="card-img-top" src="img/pih.png" alt="Card image cap" height="60%">
-      <div class="card-body">
-        <h5 class="card-title">Cat2</h5>
-        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum culpa autem dolores eaque sapiente ad.</p>
-        <a href="#" class="btn btn-primary text-center">Explore</a>
+      <div class="card col-lg-4 py-2 m-2 mx-auto" style="width: 18rem;">
+        <img class="card-img-top" src="img/pih.png " alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Collect Order</h5>
+          <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit amet, ullam explicabo tenetur possimus illo.</p>
+        </div>
+        <span>&#8594;</span>
       </div>
-    </div>
-    <div class="card  col-lg-3 py-2 m-2 mx-auto" style="width: 18rem;">
-      <img class="card-img-top" src="img/pih.png" alt="Card image cap" height="60%">
-      <div class="card-body">
-        <h5 class="card-title">Cat2</h5>
-        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum culpa autem dolores eaque sapiente ad.</p>
-        <a href="#" class="btn btn-primary text-center">Explore</a>
-      </div>
-    </div>
-    <div class="card  col-lg-3 py-2 m-2 mx-auto" style="width: 18rem;">
-      <img class="card-img-top" src="img/pih.png" alt="Card image cap" height="60%">
-      <div class="card-body">
-        <h5 class="card-title">Cat2</h5>
-        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum culpa autem dolores eaque sapiente ad.</p>
-        <a href="#" class="btn btn-primary text-center">Explore</a>
-      </div>
-    </div>
-    <div class="card  col-lg-3 py-2 m-2 mx-auto" style="width: 18rem;">
-      <img class="card-img-top" src="img/pih.png" alt="Card image cap" height="60%">
-      <div class="card-body">
-        <h5 class="card-title">Cat2</h5>
-        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum culpa autem dolores eaque sapiente ad.</p>
-        <a href="#" class="btn btn-primary text-center">Explore</a>
-      </div>
-    </div>
-    <div class="card  col-lg-3 py-2 m-2 mx-auto" style="width: 18rem;">
-      <img class="card-img-top" src="img/pih.png" alt="Card image cap" height="60%">
-      <div class="card-body">
-        <h5 class="card-title">Cat2</h5>
-        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum culpa autem dolores eaque sapiente ad.</p>
-        <a href="#" class="btn btn-primary text-center">Explore</a>
-      </div>
-    </div>
-    <div class="card  col-lg-3 py-2 m-2 mx-auto" style="width: 18rem;">
-      <img class="card-img-top" src="img/pih.png" alt="Card image cap" height="60%">
-      <div class="card-body">
-        <h5 class="card-title">Cat2</h5>
-        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum culpa autem dolores eaque sapiente ad.</p>
-        <a href="#" class="btn btn-primary text-center">Explore</a>
-      </div>
-    </div>
     </div>
   </div>
-</div>  
-  </div>
+</div>
+        </div>
+      </div>
+    </div>
     <?php
         include "include/footer.php";
     ?>
