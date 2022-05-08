6e303e681540d997fd2551e254a0b20840376653
abhishekpaneru7 <abhishek.paneru.88@gmail.com>
2022-05-08 20:40:35 +0545

    contact

--- WeB/contactus.php
+++ WeB/contactus.php
@@ -10,15 +10,30 @@
     <br>
     <br>
     <br>
-    <div class="contact-con">
+    <div class="container-fluid">
+  <h1 class="text-center pb-6 mt-5" ></h1>
+  <div class="col-8 mx-auto">
+  <div class="row text-center">
+      <div class="col-lg-4 py-2 m-2 mx-auto td-con" style="width: 21rem;">
+        <div class="card-body">
+          <h5 class="card-title">Please fill the form below!</h5>
+          <div class="name-f">
         <label for="name">Name: </label>
-        <input type="text" placeholder="Your Name Here" size="25">
+        <input type="text" placeholder="Your Name Here" size="20" class="text-center">
+        </div>
+        <div class="email-f mt-2 pb-2">
         <label for="email">Email: </label>
-        <input type="email" placeholder="Your Email Here" size="30">
+        <input type="email" placeholder="Your Email Here" size="20" class="text-center">
+        </div>
         <label for="message">Message: </label>
-        <textarea name="message" cols="50" rows="5"></textarea>
-        <input type="submit" class="btn btn-primary text-white">
+        <textarea name="message" cols="20" rows="5"></textarea>
+        <input type="submit" class="btn btn-primary text-white" style="width: 100px;">
+        </div>
+      </div>
     </div>
+  </div>
+</div>
+<br>
     <?php include "include/footer.php"; ?>
 </body>
 </html>
