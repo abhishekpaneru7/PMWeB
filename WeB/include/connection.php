0d79c0acdd0b558dedb03db67dbbfbd1132d9afa
preety934 <68704937+preety934@users.noreply.github.com>
2022-05-11 17:33:45 +0545

    Update connection.php

--- WeB/include/connection.php
+++ WeB/include/connection.php
@@ -1,7 +1,7 @@
 <?php
 // $conn = oci_connect('schema name', 'schema passowrd', 'localhost/XE');
 session_start();
-$connection = oci_connect('chf', 'localpocal', 'localhost/XE');
+$connection = oci_connect('chf_CHF', 'localpocal', 'localhost/XE');
 if (!$connection) {
     $e = oci_error();
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
