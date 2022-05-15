4e1e987487038de7e325293996e715f0bc948d36
preety934 <68704937+preety934@users.noreply.github.com>
2022-05-15 10:08:38 +0545

    DATABASE

--- Database/CHF_BACKEND.sql
+++ Database/CHF_BACKEND.sql
@@ -1,6 +1,8 @@
 DROP TABLE CUSTOMER CASCADE CONSTRAINTS;
 DROP TABLE TRADER CASCADE CONSTRAINTS;
 DROP TABLE PRODUCT CASCADE CONSTRAINTS;
+DROP TABLE SHOP CASCADE CONSTRAINTS;
+
 
 Create table customer (
     customer_id number(5) primary key,
@@ -22,6 +24,12 @@ CREATE TABLE TRADER(
     password VARCHAR2(50) not null
 );
 
+CREATE TABLE SHOP(
+shop_id number(5) PRIMARY KEY,
+tradertype VARCHAR2(15) NOT NULL UNIQUE
+--trader_id NUMBER(10) REFERENCES TRADER(TRADER_ID)
+);
+
 CREATE TABLE PRODUCT(
     product_id number(5) primary key,
     product_title VARCHAR2(50) not null,
@@ -29,5 +37,7 @@ CREATE TABLE PRODUCT(
     description VARCHAR2(500),
     price number(8,3) not null,
     quantity number(3) not null,
-    image VARCHAR2(255) not null
+    image VARCHAR2(255) not null,
+    shop_id number(5) REFERENCES SHOP(shop_id)
+    
 );
