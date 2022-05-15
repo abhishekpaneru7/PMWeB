d3d16482eaf82d712e94c7a2d1dfa9feaa034584
Pravat Lama <pravatlama00@gmail.com>
2022-05-15 20:05:21 +0545

    sort

--- WeB/shop.php
+++ WeB/shop.php
@@ -41,275 +41,60 @@
       <div class="shop-flex">
         <div class="sort-shop">
           <h3>SORT BY:</h3>
-          <hr class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 120px; background-color: #7c4dff; height: 2px">
-          <!-- <h5 class="text-center">NAME</h5> -->
-          <!-- <form class="d-flex" action="" method="post">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <a href="shop.php" type="button search-btn" class="btn btn-primary" name="btnSearch">
-                <i class="bi bi-search"></i>
-              </a>
-            </form> -->         
-        <br>      
-        
-        <form action="" method="POST"> 
-        <label>Shop Type:</label>
-        <select name = "trader" id="trader">
-            <option value="" hidden selected disabled>--Please select--</option>
-            <option value="Bakery" <?php 
-                if (isset($_POST['trader']) && ($_POST['trader'] == 'Bakery')) 
-                    echo 'selected'; ?>
-                >Bakery</option>
-            <option value="Butcher" <?php 
-                if (isset($_POST['trader']) && ($_POST['trader'] == 'Butcher')) 
-                    echo 'selected'; ?>
-                >Butcher</option>
-            <option value="fishmonger" <?php 
-                if (isset($_POST['trader']) && ($_POST['trader'] == 'Fishmonger')) 
-                    echo 'selected'; ?>
-                >Fishmonger</option>
-                <option value="greengrocer" <?php 
-                if (isset($_POST['trader']) && ($_POST['trader'] == 'Greengrocher')) 
-                    echo 'selected'; ?>
-                >Greengrocher</option>
-                <option value="delicatessen" <?php 
-                if (isset($_POST['trader']) && ($_POST['trader'] == 'Delicatessen')) 
-                    echo 'selected'; ?>
-                >Fishmonger</option>
-        </select><br/><br>
-
-          <div class="category">
-          <label>Category:</label>  <br>
-          <input type="checkbox" name="Category" value="redmeat"<?php
-          if (isset($_POST['Category']) && ($_POST['Category'] == 'RED MEAT')) 
-          echo 'checked="checked"';
-          ?>>
-          <label for="meat"> Red Meat </label>
-          <br>
-          <input type="checkbox" name="Category" value="whitemeat"<?php
-          if (isset($_POST['Category']) && ($_POST['Category'] == 'WHITE MEAT')) 
-          echo 'checked="checked"';
-          ?>>
-          <label for="meat"> White Meat </label>
-          <br>
-          <input type="checkbox" name="Category" value="fruit"<?php
-          if (isset($_POST['Category']) && ($_POST['Category'] == 'FRUIT')) 
-          echo 'checked="checked"';
-          ?>>
-          <label for="fruit"> Fruit </label>
-          <br>
-          <input type="checkbox" name="Category" value="vegetable"<?php
-          if (isset($_POST['Category']) && ($_POST['Category'] == 'VEGETABLE')) 
-          echo 'checked="checked"';
-          ?>>
-          <label for="vegetable">Vegetable</label>
-          <br>
-          <input type="checkbox" name="Category" value="baked item"<?php
-          if (isset($_POST['Category']) && ($_POST['Category'] == 'BAKED ITEMS')) 
-          echo 'checked="checked"';
-          ?>>
-          <label for="fish">Fish</label>
-          <br>
-          <input type="checkbox" name="Category" value="Fish"<?php
-          if (isset($_POST['Category']) && ($_POST['Category'] == 'FISH')) 
-          echo 'checked="checked"';
-          ?>>
-          <label for="shellfish">Shellfish</label>
-          <br>
-          <input type="checkbox" name="Category" value="shellfish"<?php
-          if (isset($_POST['Category']) && ($_POST['Category'] == 'SHELLFISH')) 
-          echo 'checked="checked"';
-          ?>>
-          <label for="baked item"> Baked items </label><br>
-          <input type="checkbox" name="Category" value="ready-to-eat item"<?php
-          if (isset($_POST['Category']) && ($_POST['Category'] == 'READY TO EAT ITEMS')) 
-          echo 'checked="checked"';
-          ?>>
-          <label for="ready-to-eat item"> Ready to eat items </label>
-          
-          </div><br>
-        
-        <div class="sort">
-        <label>Sort by:</label>  <br>      
-            <input type="radio" name="sortby" value="sortAtoZ" <?php
-                if (isset($_POST['sortby']) && ($_POST['sortby'] == 'sortAtoZ')) 
-                    echo ' checked="checked"'; 
-                ?> />Name, A to Z<br/>
-            <input type="radio" name="sortby" value="sortZtoA" <?php
-                if (isset($_POST['sortby']) && ($_POST['sortby'] == 'sortZtoA')) 
-                    echo ' checked="checked"'; 
-                ?> />Name, Z to A<br/>
-            <input type="radio" name="sortby" value="sortLowtoHigh" <?php
-                if (isset($_POST['sortby']) && ($_POST['sortby'] == 'sortLowtoHigh')) 
-                    echo ' checked="checked"'; 
-                ?> />Price, Low to High<br/>
-            <input type="radio" name="sortby" value="sortHightoLow" <?php
-                if (isset($_POST['sortby']) && ($_POST['sortby'] == 'sortHightoLow')) 
-                    echo ' checked="checked"'; 
-                ?> 
-                />Price, High to Low<br/>
-                </div>
-                <br>
-                <button type="submit" name="btnSubmit">SORT BY</button>
-          <br>
-          <div class="text-center">
-          <h5 class="text-center">Shop Type</h5>
-          <input type="radio" name="trader" value="bakery">
-          <label for="bakery"> Bakery </label>
-          <br>
-          <input type="radio" name="trader" value="butcher">
-          <label for="butcher"> Butcher </label>
-          <br>
-          <input type="radio" name="trader" value="fishmonger">
-          <label for="fishmonger"> FishMonger </label>
-          <br>
-          <input type="radio" name="trader" value="greengrocher">
-          <label for="greengrocher">Green Grocher</label>
-          <br>
-          <input type="radio" name="trader" value="delicatessen">
-          <label for="delicatessen"> Delicatessen </label>
-          </div>
-          <br>
-          <br>
-          <h5 class="text-center">ALPHABET</h5>
-          &nbsp; &nbsp; &nbsp; &nbsp;
-          <input type="radio" name="alphabet" value="AESC" id="sort">
-          <label for="AESC"> AESC </label>
-          &nbsp; &nbsp; &nbsp; &nbsp;
-          <input type="radio" name="alphabet" value="DESC" id="sort">
-          <label for="DESC"> DESC </label>
-          <br>
-          <br>
-          <h5 class="text-center">PRICE</h5>
-          <div class="flex-search">
-            <input type="number" placeholder="Min-Price" style="width: 120px;">
-            <span>&#8594;</span>
-            <input type="number" placeholder="Max-Price" style="width: 120px;">
-        </div><br><br>
-        
+          <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 120px background-color: #7c4dff; height: 2px"><br>      
+          <form action="shop.php" method="post">
+            <label>Shop Type:</label>
+            <select name="trader">
+              <option disabled selected>--Please Select--</option>
+              <option value="Butcher" <?php echo isset($_POST['trader']) && $_POST['trader'] === 'Butcher' ? 'selected' : ''; ?>>Butcher</option>
+              <option value="Bakery" <?php echo isset($_POST['trader']) && $_POST['trader'] === 'Bakery' ? 'selected' : ''; ?>>Bakery</option>
+              <option value="Fishmonger" <?php echo isset($_POST['trader']) && $_POST['trader'] === 'Fishmonger' ? 'selected' : ''; ?>>Fishmonger</option>
+              <option value="Greengrocher" <?php echo isset($_POST['trader']) && $_POST['trader'] === 'Greengrocher' ? 'selected' : ''; ?>>Greengrocher</option>
+              <option value="Dellicatessen" <?php echo isset($_POST['trader']) && $_POST['trader'] === 'Dellicatessen' ? 'selected' : ''; ?>>Dellicatessen</option>
+            </select><br><br>
+            <?php
+              if(isset($_POST['trader']) && $_POST['trader'] === 'Butcher'){
+                echo '<label for="category">Category:</label><br>';
+                echo '<input type="checkbox" name="category[]" value="red meat"';
+                echo '> Red Meat<br>';
+                echo '<input type="checkbox" name="category[]" value="white meat"';
+                echo '> White Meat<br><br>';
+              }
+              if(isset($_POST['trader']) && $_POST['trader'] === 'Greengrocher'){
+                echo '<input type="checkbox" name="category[]" value="fruit"';
+                echo '> Fruits<br>';
+                echo '<input type="checkbox" name="category[]" value="vegetable"';
+                echo '> Vegetable<br><br>';
+              }
+              if(isset($_POST['trader']) && $_POST['trader'] === 'Fishmonger'){
+                echo '<input type="checkbox" name="category[]" value="fish"';
+                echo '> Fish<br>';
+                echo '<input type="checkbox" name="category[]" value="shellfish"';
+                echo '> Shellfish<br><br>';
+              }
+            ?>
+            <label for="sort">Sort by:</label><br>
+            <input type="radio" name="sort" value="asc" <?php echo (isset($_POST['sort']) && $_POST['sort'] == 'asc') ? "checked = checked" : '';?>> Name A to Z<br>
+            <input type="radio" name="sort" value="desc" <?php echo (isset($_POST['sort']) && $_POST['sort'] == 'desc') ? "checked = checked" : '';?>> Name Z to A<br>
+            <input type="radio" name="sort" value="low" <?php echo (isset($_POST['sort']) && $_POST['sort'] == 'low') ? "checked = checked" : '';?>> Price low to high<br>
+            <input type="radio" name="sort" value="high" <?php echo (isset($_POST['sort']) && $_POST['sort'] == 'high') ? "checked = checked" : '';?>> Price high to low<br><br>
+            <label for="price">Price Range:</label><br>
+            <input type="number" name="min" placeholder="Min-Price" style="width: 120px;">
+            <span>&#8594</span>
+            <input type="number" name="max" placeholder="Max-Price" style="width: 120px;">
+            <br><br><button type="submit" name="btnFilter">SORT BY</button>
+          </form>
         </div>
-        </form>
-        
-        <?php
-          if (isset($_POST['btnSubmit'])){
-           //Since we have not inserted data for trader it wont work
-            $trader =trim($_POST['trader']);
-            if($trader=="BUTCHER"){
-                $sql="SELECT * FROM product where SHOPID= '1'";
-            }elseif($trader=="BAKERY"){
-              $sql="SELECT * FROM product where SHOPID= '2'";
-            }elseif($trader=="GREENGROCHER"){
-              $sql="SELECT * FROM product where SHOPID= '3'";
-            }elseif($trader=="FISHMONGER"){
-              $sql="SELECT * FROM product where SHOPID= '4'";
-            }elseif($trader=="Delicatessen"){
-              $sql="SELECT * FROM product where SHOPID= '5'";
-            }else{
-              $sql ="SELECT * FROM product";
-            }
-
-          $category =trim($_POST['Category']?:'');
-            if($category=="WHITE MEAT"){
-                $sql="SELECT * FROM product where CATEGORY='WHITE MEAT'";
-            }elseif($category=="RED MEAT"){
-                $sql="SELECT * FROM product where CATEGORY='RED MEAT'";
-            }elseif($category=="FRUIT"){
-                $sql="SELECT * FROM product where CATEGORY='FRUIT'";
-            }elseif($category=="VEGETABLE"){
-                $sql="SELECT * FROM product where CATEGORY='VEGETABLE'";
-              }elseif($category=="FISH"){
-                $sql="SELECT * FROM product WHERE CATEGORY = 'FISH'"; 
-              }elseif($category=="SHELLFISH"){
-                $sql="SELECT * FROM product WHERE CATEGORY = 'SHELLFISH'";  
-            }elseif($category=="BAKED ITEMS"){
-                $sql="SELECT * FROM product WHERE CATEGORY = 'BAKED ITEMS'";
-            }elseif($category=="READY TO EAT ITEMS"){
-              $sql="SELECT * FROM product WHERE CATEGORY = 'READY TO EAT ITEMS'";            
-            }else{
-            $sql = "SELECT * FROM product";
-        }
-        $sort = isset($_POST['sortby']) ? $_POST['sortby'] : 'default_value';
-
-      
-      if(!empty($sort)){
-        if ($sort=='sortAtoZ'){
-            $sql.=" order by PRODUCT_TITLE ASC";
-        }
-        else if ($sort=='sortZtoA'){
-            $sql.=" order by PRODUCT_TITLE DESC";
-        }
-        else if ($sort=='sortLowtoHigh'){
-             $sql.=" order by PRICE ASC";
-        }
-        else if ($sort=='sortHightoLow'){
-            $sql.=" order by PRICE DESC";
-        }
-    }
-    
-  }
-         ?>
         <div class="shop-products">
-        <div class="container-fluid">
-  <h1 class="text-center pb-4 mt-4" >PRODUCTS</h1>
-  <div class="col-10 mx-auto">
-  <div class="row text-center">
-   
-    <?php
-        $sql = 'Select * from product';
-        $query = oci_parse($connection, $sql);
-        oci_execute($query);
-          while($row = oci_fetch_assoc($query)){
-            $id = $row['PRODUCT_ID'];
-            echo '<div class="card col-lg-4 py-2 m-2 mx-auto" style="width: 18rem;">';
-              echo '<img src="./products/' .$row['IMAGE'] . '"alt="Card image cap" height="262px" class="card-img-top">';
-              echo '<div class="card-body">';
-                echo '<h5 class="cart-title">' . $row['PRODUCT_TITLE'] . '</h5>';
-                echo '<h5 class="cart-title">£' . $row['PRICE'] . '</h5>';
-                // @parvat-be sure to make make definite size for product details so that all containers
-                echo '<p class="card-text">' . $row['DESCRIPTION'] . '</p>';
-                echo '</div>';
-                echo '<a href="productdetail.php" class="btn btn-primary btn-card" style="margin: 0 24px;">View Product</a>';
-            echo '</div>';
-          }
-      
-      ?>
-      <!-- <div class="card col-lg-4 py-2 m-2 mx-auto" style="width: 18rem;">
-        <img class="card-img-top" src="img/pih.png" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Rs: 100</h5>
-          <h5 class="card-title">Food</h5>
-          <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eaque aspernatur quam repudiandae commodi dolorum!</p>
-          <a href="productdetail.php" class="btn btn-primary btn-card">View Product</a>
-        </div>
-      </div>
-      <div class="card col-lg-4 py-2 m-2 mx-auto" style="width: 18rem;">
-        <img class="card-img-top" src="img/pih.png" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Make Payment</h5>
-          <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim dicta dolorum reprehenderit reiciendis impedit distinctio.</p>
-        </div>
-        <span>&#8594;</span>
-      </div>
-      <div class="card col-lg-4 py-2 m-2 mx-auto" style="width: 18rem;">
-        <img class="card-img-top" src="img/pih.png" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Make Payment</h5>
-          <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim dicta dolorum reprehenderit reiciendis impedit distinctio.</p>
-        </div>
-        <span>&#8594;</span>
-      </div>
-      <div class="card col-lg-4 py-2 m-2 mx-auto" style="width: 18rem;">
-        <img class="card-img-top" src="img/pih.png " alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Collect Order</h5>
-          <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit amet, ullam explicabo tenetur possimus illo.</p>
-        </div>
-        <span>&#8594;</span>
-      </div> -->
-    </div>
-  </div>
-</div>
-        </div>
+          <div class="container-fluid">
+            <h1 class="text-center pb-4 mt-4" >PRODUCTS</h1>
+            <div class="col-10 mx-auto">
+              <div class="row text-center">
+                <?php $sql = 'Select * from product'; include './include/sortfunction.php'; ?>
+              </div>
+            </div>
+          </div>
+       </div>
       </div>
     </div>
     <?php
