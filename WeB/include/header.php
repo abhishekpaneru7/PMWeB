4b09484213c87a820ef7491403e7ce72d1c8687a
preety934 <68704937+preety934@users.noreply.github.com>
2022-05-21 13:40:49 +0545

    logout session

--- WeB/include/header.php
+++ WeB/include/header.php
@@ -43,7 +43,7 @@
               }else{
                 ?>
               <!-- @preeti-yo line ta mathi ko &nbsp; lai if else ma hala ani if session set xa bhane dekhaune natra na dekhaune bhanau thnx.. -->
-              <a href="logout.php" class="btn btn-dark btn-nav text-center" onMouseOver="this.style.color='#E09C78'" onMouseOut="this.style.color='white'">Sign Out</a>
+              <a href="signout.php" class="btn btn-dark btn-nav text-center" onMouseOver="this.style.color='#E09C78'" onMouseOut="this.style.color='white'">Sign Out</a>
               <?php } ?>
             </ul>
           </div>
