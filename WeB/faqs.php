7e792da65771d76cacf623c4b9fb758adcbce9b6
Sakchyam-ss <66342221+Sakchyam-ss@users.noreply.github.com>
2022-05-25 15:50:21 +0545

    Faqs

--- WeB/faqs.php
+++ WeB/faqs.php
@@ -24,49 +24,60 @@
     </div>
     <div class="faq-con">
         <div class="faq-qa">
-            <h5>?</h5>
-            <p>ANS</p>
+            <h5>What is CHF Local's Mart?</h5>
+            <p>Answer -  CHF Local's Mart is a collaborative group of local grocery stores from  Cleckhuddersfax with a motive to provide a hassle free online grocery store at peoples own convenince.
+            </p>
         </div>
         <div class="faq-qa">
-            <h5>?</h5>
-            <p>ANS</p>
+            <h5>Why use our service?</h5>
+            <p>Answer - We aim to make grocery shopping convenient and quick with online options on transactions</p>
         </div>
         <div class="faq-qa">
-            <h5>?</h5>
-            <p>ANS</p>
+            <h5>Is it necessary to have an account to shop on CHF Local Mart?</h5>
+            <p>Answer - Yes, you need to have an account to shop on CHF Local Mart.</p>
+            <p>To register your account you can follow the following steps: </p>
+            <p>Steps: <br>
+
+                1.Go to the Login page.<br>
+                2.At the bottom, you can see "Not registered yet? Signup now"<br>
+
+                3.Click on Signup now and can register your account</p>
         </div>
         <div class="faq-qa">
-            <h5>?</h5>
-            <p>ANS</p>
+            <h5>Are there any delivery cost?</h5>
+            <p>Answer - No, there are not as you need to go and collect your orders yourself from the collection slot that you chose.</p>
         </div>
         <div class="faq-qa">
-            <h5>?</h5>
-            <p>ANS</p>
+            <h5>How to know the items available on my cart?</h5>
+            <p>Answer - Steps:<br>
+                1. Go to the Login of www.chflocalmart.com<br>
+                2. Click on the Cart icon and you can now see the items available.</p>
         </div>
         <div class="faq-qa">
-            <h5>?</h5>
-            <p>ANS</p>
+            <h5>Can we return the goods if they are expired?</h5>
+            <p>Answer - Yes, you can return within 3 days of order placement. However, Fresh products that are subjected to decay cannot be returned</p>
         </div>
         <div class="faq-qa">
-            <h5>?</h5>
-            <p>ANS</p>
+            <h5>If I missed collecting orders from the delivery slot. What should I do?</h5>
+            <p>Answer - If someone missed collecting an order from the collection slot. They will not be refunded but they have a small window to collect the same product if failed to collect their time.</p>
         </div>
         <div class="faq-qa">
-            <h5>?</h5>
-            <p>ANS</p>
+            <h5>Can I get a refund if my order gets canceled?</h5>
+            <p>Answer - Yes, you are refunded if orders get canceled. For the refund, you will be contacted by our marketing team, or you call us at + 01 234 567 89.</p>
         </div>
         <div class="faq-qa">
-            <h5>?</h5>
-            <p>ANS</p>
+            <h5>Are there any hidden costs or charges if I order from CHF Local Mart?</h5>
+            <p>Answer - No, there are no hidden costs or charges when you order from CHF Local Mart. All the costs are 100% visible at the end of the checkout.</p>
         </div>
         <div class="faq-qa">
-            <h5>?</h5>
-            <p>ANS</p>
+            <h5>Are the prices negotiable?</h5>
+            <p>Answer- The product prices are not negotiable. We provide the best quality of goods without any added costs and provide you with the best deals and offers while keeping the shops running.</p>
         </div>
         <div class="faq-qa">
-            <h5>?</h5>
-            <p>ANS</p>
+            <h5>How will I know my order has been confirmed?</h5>
+            <p>Answer - After you checkout, you need to pay the total amount through Paypal. After the payment, you will receive an invoice that confirms your order.</p>
         </div>
+
     </div>
     
     <?php include "include/footer.php" ?>
