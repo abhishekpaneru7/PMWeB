fbe2bd477c231637461953b7a5731713f2311311
abhishekpaneru7 <abhishek.paneru.88@gmail.com>
2022-05-25 22:11:35 +0545

    cart

--- WeB/cart.php
+++ WeB/cart.php
@@ -57,21 +57,48 @@
                 </table>
             </div>
             <div class="cart-sum" style="border: 2px solid black;">
-                <div class="container-fluid" style="width: 21rem;">
+                <div class="container-fluid mx-auto " style="width: 21rem;">
                     <h3 class="text-center mt-3">SUMMARY</h3>
                     <div class="card-body">
                         <h5 class="card-title text-center"><?php echo "Total items(" . $totalQuantity . ")"; ?></h5>
-                        <div class="">
+                        <!-- <div class="">
                             <form class="d-flex">
                                 <input class="form-control me-2" type="search" placeholder="Enter Discount Code" aria-label="Search">
                                 <button type="button search-btn" class="btn btn-primary">
                                 APPLY
                             </form>
-                        </div>
+                        </div> -->
                         <br>
-                        <h6 class="card-title text-center">Discount: </h6>
+                        <h6 class="card-title text-center">V.A.T.: </h6>
                         <hr>
                         <h6 class="cart-title text-center"><?php echo "Grand Total: £" . $grand; ?></h6>
+                        <br>
+                        <h6 class="card-title text-center">Choose Collection Slot:</h6>
+                        <hr>
+                        <div class="text-center">
+                            <input type="radio" name="c-s-name" value="WeD">
+                            <label for="WeD">Wednesday</label>
+                            <input type="radio" name="c-s-name" value="ThU">
+                            <label for="WeD">Thursday</label>
+                            <input type="radio" name="c-s-name" value="FrI">
+                            <label for="WeD">Friday</label>
+                        </div>
+                        <br>
+                        <div class="text-center">
+                            <input type="radio" name="c-s-time" value="10-1">
+                            <label for="WeD">10A.M. - 1P.M.</label>
+                            <input type="radio" name="c-s-time" value="1-4">
+                            <label for="WeD">1P.M. - 4P.M.</label>
+                            <input type="radio" name="c-s-time" value="4-7">
+                            <label for="WeD">4P.M. - 7P.M.</label>
+                        </div>
+                        <br>
+                        <h6 class="card-title text-center">Choose Payment Method:</h6>
+                        <div class="text-center">
+                            PayPal ImG
+                        </div>
+                        <br>
+                        <hr>
                         <div class="text-center">
                         <a href="#" class="btn btn-danger btn-card">Check OUT</a>
                         </div>
@@ -80,6 +107,9 @@
             </div>
         </div>
         <br>
+    </div>
+    <div class="c-s-btn">
+        <a href="" class="btn btn-primary text-white btn-card btn-cs">Continue Shopping</a>
     </div>
     <?php } include "include/footer.php"; ?>
 </body>
