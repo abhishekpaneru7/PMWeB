1c9971c5ef71e0d4fbf0667d009f0767261a1e51
Pravat Lama <pravatlama00@gmail.com>
2022-06-01 23:55:15 +0545

    cart maximum order limit 20 updated

--- WeB/include/addtocart.php
+++ WeB/include/addtocart.php
@@ -8,24 +8,36 @@
   else{
     $id = $_SESSION['id'];
   }
- $prodid = $_GET['id'];
- $custid = $_SESSION['id'];
- $quantity = 1;
- $sql = "Select * from product where product_id = $prodid";
- $result = oci_parse($connection, $sql);
- oci_execute($result);
- while($row = oci_fetch_assoc($result)){
-  $price = $row['PRICE'];
-  $sql1 = "insert into cart(CUSTOMER_ID, PRODUCT_ID, QUANTITY, TOTAL) values($custid, $prodid, $quantity, ($price * $quantity))";
-  $insert = oci_parse($connection, $sql1);
-  if(oci_execute($insert)){
-    echo "<script>alert(\"Item added to cart.\");";
-    echo "history.go(-1);";
-    echo "</script>";
-  }else{
-   echo "<script>alert(\"Item already added to cart.\");";
-   echo "history.go(-1);";
-   echo "</script>";
-  }
+ $sql0 = "Select SUM(QUANTITY) from CART";
+ $result0 = oci_parse($connection,$sql0);
+ oci_execute($result0);
+ while($row = oci_fetch_assoc($result0)){
+   $count = $row['SUM(QUANTITY)'];
+   if($count < 20){
+    $prodid = $_GET['id'];
+    $custid = $_SESSION['id'];
+    $quantity = 1;
+    $sql = "Select * from product where product_id = $prodid";
+    $result = oci_parse($connection, $sql);
+    oci_execute($result);
+    while($row = oci_fetch_assoc($result)){
+      $price = $row['PRICE'];
+      $sql1 = "insert into cart(CUSTOMER_ID, PRODUCT_ID, QUANTITY, TOTAL) values($custid, $prodid, $quantity, ($price * $quantity))";
+      $insert = oci_parse($connection, $sql1);
+      if(oci_execute($insert)){
+        echo "<script>alert(\"Item added to cart.\");";
+        echo "history.go(-1);";
+        echo "</script>";
+      }else{
+        echo "<script>alert(\"Item already added to cart.\");";
+        echo "history.go(-1);";
+        echo "</script>";
+      }
+    }
+   }else{
+      echo "<script>alert(\"Maximum cart limit is reached (20 items).\");";
+      echo "history.go(-1);";
+      echo "</script>";
+   }
  }
 ?>
